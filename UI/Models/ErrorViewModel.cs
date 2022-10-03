822f3b2608acb7390f068978fea9319492c0eaca
laurens <lhh.weterings@student.avans.nl>
2022-10-03 10:04:35 +0200

    init

---
+++ UI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Avans_NoWaste.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
