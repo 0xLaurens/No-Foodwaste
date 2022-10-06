a7703c44d87f3ce47d690d3e200eefabe7a75b27
lhh.weterings <lhh.weterings@student.avans.nl>
2022-10-06 09:52:25 +0200

    student validation added

--- Domain/Student.cs
+++ Domain/Student.cs
@@ -5,8 +5,14 @@ namespace Domain;
 public class Student
 {
     public int StudentId { get; set; }
+    [Required(ErrorMessage = "Please enter your email address")]
+    [EmailAddress]
     public string EmailAddress { get; set; }
+    [Required(ErrorMessage = "Please select a city you currently study at")]
     public City? City { get; set; }
+    [Required(ErrorMessage = "Please enter a valid phone number")] 
+    [Phone] 
     public string? PhoneNumber { get; set; }
-    public DateTime? DateOfBirth { get; set; }
+    [Required(ErrorMessage = "Enter your Date of Birth")]
+    public DateOnly? DateOfBirth { get; set; }
 }
