06c60410897ed148092b56b97f495c1a0bb1a23c
lhh.weterings <lhh.weterings@student.avans.nl>
2022-10-06 15:36:18 +0200

    Migration

--- Domain/Cafeteria.cs
+++ Domain/Cafeteria.cs
@@ -5,6 +5,6 @@ namespace Domain;
 public class Cafeteria
 {
     public int CafeteriaId { get; set; }
-    public City City { get; set; }
-    public Location Location { get; set; } 
+    public City? City { get; set; }
+    public Location? Location { get; set; } 
 }
