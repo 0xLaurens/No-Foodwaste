96e0b16cf9e817f028cc3741ba47015c9753abe7
lhh.weterings <lhh.weterings@student.avans.nl>
2022-10-08 20:46:23 +0200

    Infrastructure CafeteriaRepo test

---
+++ Infrastructure.Tests/CafeteriaRepositoryTest.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace Infrastructure.Test;
+
+public class CafeteriaRepositoryTest 
+{
+    [Fact]
+    public void GetCafeteria()
+    {
+        // Arrange
+        
+        // Act
+        
+        // Assert
+    }
+}
