c1ae0e53729de6d744ce827b479ff84c3c4758a7
lhh.weterings <lhh.weterings@student.avans.nl>
2022-10-10 18:07:25 +0200

    Matching page for the user will be fetched

--- Portal/Controllers/HomeController.cs
+++ Portal/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Diagnostics;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using Avans_NoWaste.Models;
 using DomainServices.Repos.Inf;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace Avans_NoWaste.Controllers;
 
@@ -10,18 +12,30 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly IPackageRepository _packageRepository;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly IStudentRepository _studentRepository;
 
-    public HomeController(ILogger<HomeController> logger, IPackageRepository packageRepository)
+
+    public HomeController(ILogger<HomeController> logger, IPackageRepository packageRepository, UserManager<IdentityUser> userManager, IStudentRepository studentRepository)
     {
+        _studentRepository = studentRepository;
         _logger = logger;
         _packageRepository = packageRepository;
+        _userManager = userManager;
     }
 
     [Authorize]
     public IActionResult Index()
     {
-        
         return View(_packageRepository.GetNonReservedPackages());
+    }
+    
+    [Authorize]
+    public IActionResult Orders()
+    {
+        var email = this.User.FindFirstValue(ClaimTypes.Email);
+        var student = _studentRepository.GetStudentByEmail(email);
+        return View(_packageRepository.GetPackagesByStudent(student.StudentId));
     }
 
     [Authorize]
