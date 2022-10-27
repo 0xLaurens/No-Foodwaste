692e173baa0e52fb427a86a6bb727e897fe15e64
lhh.weterings <lhh.weterings@student.avans.nl>
2022-10-27 19:34:15 +0200

    Restfull

---
+++ Infrastructure/Migrations/20221027150317_updatedModels.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class updatedModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cafeterias_Cities_CityId",
+                table: "Cafeterias");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Cities_CityId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_CityId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cafeterias_CityId",
+                table: "Cafeterias");
+
+            migrationBuilder.UpdateData(
+                table: "Packages",
+                keyColumn: "PackageId",
+                keyValue: 1,
+                columns: new[] { "EndTimeSlot", "StartTimeSlot" },
+                values: new object[] { new DateTime(2022, 10, 27, 20, 3, 17, 214, DateTimeKind.Local).AddTicks(9384), new DateTime(2022, 10, 27, 17, 3, 17, 214, DateTimeKind.Local).AddTicks(9370) });
+
+            migrationBuilder.UpdateData(
+                table: "Packages",
+                keyColumn: "PackageId",
+                keyValue: 2,
+                columns: new[] { "EndTimeSlot", "StartTimeSlot" },
+                values: new object[] { new DateTime(2022, 10, 27, 20, 3, 17, 214, DateTimeKind.Local).AddTicks(9446), new DateTime(2022, 10, 27, 17, 3, 17, 214, DateTimeKind.Local).AddTicks(9426) });
+
+            migrationBuilder.UpdateData(
+                table: "Packages",
+                keyColumn: "PackageId",
+                keyValue: 3,
+                columns: new[] { "EndTimeSlot", "StartTimeSlot" },
+                values: new object[] { new DateTime(2022, 10, 27, 20, 3, 17, 214, DateTimeKind.Local).AddTicks(9478), new DateTime(2022, 10, 27, 17, 3, 17, 214, DateTimeKind.Local).AddTicks(9464) });
+
+            migrationBuilder.UpdateData(
+                table: "Packages",
+                keyColumn: "PackageId",
+                keyValue: 4,
+                columns: new[] { "EndTimeSlot", "StartTimeSlot" },
+                values: new object[] { new DateTime(2022, 10, 27, 20, 3, 17, 214, DateTimeKind.Local).AddTicks(9511), new DateTime(2022, 10, 27, 17, 3, 17, 214, DateTimeKind.Local).AddTicks(9497) });
+
+            migrationBuilder.UpdateData(
+                table: "Packages",
+                keyColumn: "PackageId",
+                keyValue: 5,
+                columns: new[] { "EndTimeSlot", "StartTimeSlot" },
+                values: new object[] { new DateTime(2022, 10, 27, 20, 3, 17, 214, DateTimeKind.Local).AddTicks(9544), new DateTime(2022, 10, 27, 17, 3, 17, 214, DateTimeKind.Local).AddTicks(9530) });
+
+            migrationBuilder.UpdateData(
+                table: "Packages",
+                keyColumn: "PackageId",
+                keyValue: 6,
+                columns: new[] { "EndTimeSlot", "StartTimeSlot" },
+                values: new object[] { new DateTime(2022, 10, 27, 20, 3, 17, 214, DateTimeKind.Local).AddTicks(9579), new DateTime(2022, 10, 27, 17, 3, 17, 214, DateTimeKind.Local).AddTicks(9564) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Packages",
+                keyColumn: "PackageId",
+                keyValue: 1,
+                columns: new[] { "EndTimeSlot", "StartTimeSlot" },
+                values: new object[] { new DateTime(2022, 10, 26, 17, 39, 19, 820, DateTimeKind.Local).AddTicks(6671), new DateTime(2022, 10, 26, 14, 39, 19, 820, DateTimeKind.Local).AddTicks(6657) });
+
+            migrationBuilder.UpdateData(
+                table: "Packages",
+                keyColumn: "PackageId",
+                keyValue: 2,
+                columns: new[] { "EndTimeSlot", "StartTimeSlot" },
+                values: new object[] { new DateTime(2022, 10, 26, 17, 39, 19, 820, DateTimeKind.Local).AddTicks(6728), new DateTime(2022, 10, 26, 14, 39, 19, 820, DateTimeKind.Local).AddTicks(6709) });
+
+            migrationBuilder.UpdateData(
+                table: "Packages",
+                keyColumn: "PackageId",
+                keyValue: 3,
+                columns: new[] { "EndTimeSlot", "StartTimeSlot" },
+                values: new object[] { new DateTime(2022, 10, 26, 17, 39, 19, 820, DateTimeKind.Local).AddTicks(6761), new DateTime(2022, 10, 26, 14, 39, 19, 820, DateTimeKind.Local).AddTicks(6747) });
+
+            migrationBuilder.UpdateData(
+                table: "Packages",
+                keyColumn: "PackageId",
+                keyValue: 4,
+                columns: new[] { "EndTimeSlot", "StartTimeSlot" },
+                values: new object[] { new DateTime(2022, 10, 26, 17, 39, 19, 820, DateTimeKind.Local).AddTicks(6799), new DateTime(2022, 10, 26, 14, 39, 19, 820, DateTimeKind.Local).AddTicks(6780) });
+
+            migrationBuilder.UpdateData(
+                table: "Packages",
+                keyColumn: "PackageId",
+                keyValue: 5,
+                columns: new[] { "EndTimeSlot", "StartTimeSlot" },
+                values: new object[] { new DateTime(2022, 10, 26, 17, 39, 19, 820, DateTimeKind.Local).AddTicks(6833), new DateTime(2022, 10, 26, 14, 39, 19, 820, DateTimeKind.Local).AddTicks(6818) });
+
+            migrationBuilder.UpdateData(
+                table: "Packages",
+                keyColumn: "PackageId",
+                keyValue: 6,
+                columns: new[] { "EndTimeSlot", "StartTimeSlot" },
+                values: new object[] { new DateTime(2022, 10, 26, 17, 39, 19, 820, DateTimeKind.Local).AddTicks(6871), new DateTime(2022, 10, 26, 14, 39, 19, 820, DateTimeKind.Local).AddTicks(6857) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_CityId",
+                table: "Students",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cafeterias_CityId",
+                table: "Cafeterias",
+                column: "CityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cafeterias_Cities_CityId",
+                table: "Cafeterias",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "CityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Cities_CityId",
+                table: "Students",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "CityId");
+        }
+    }
+}
---
+++ WebService/Models/Package/NewPackageDto.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using Domain;
+
+namespace WebService.Models.Package;
+
+public class NewPackageDto
+{
+
+    [Required(ErrorMessage = "Enter a unique name")]
+    public string? Name { get; set; }
+
+    public byte[]? Thumbnail { get; set; }
+    public string? ThumbnailFormat { get; set; }
+    [Required] public int? CityId { get; set; }
+
+    [Required] public int CafeteriaId { get; set; }
+
+    [Required(ErrorMessage = "Enter a valid start time")]
+    public DateTime StartTimeSlot { get; set; }
+
+    [Required(ErrorMessage = "Enter a valid end time")]
+    public DateTime EndTimeSlot { get; set; }
+
+    public bool EighteenPlus { get; set; }
+
+    [Required(ErrorMessage = "Enter a valid price ex: 1.99")]
+    public decimal? Price { get; set; }
+
+    public Category? Category { get; set; }
+    public int? StudentId { get; set; }
+}
