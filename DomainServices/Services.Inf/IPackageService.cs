523b9d4a445223a912f8c866c3ab4602d3c268fd
lhh.weterings <lhh.weterings@student.avans.nl>
2022-10-29 14:50:20 +0200

    Package detail page

--- DomainServices/Services.Inf/IPackageService.cs
+++ DomainServices/Services.Inf/IPackageService.cs
@@ -6,11 +6,11 @@ public interface IPackageService
 {
     public bool CanPackageBeAltered(Package package);
     public bool PackagesHasProductThatContainsAlcohol(Package package);
+    public bool PackageDateNotTooFarInTheFutureOrPast(Package package);
+    public bool PackageHasCorrectStartAndEnd(Package package);
+    public bool PackageHasCorrectDate(Package package);
+    
     public bool CanPackageBeReservedByStudent(Package package, Student student);
     public bool StudentOldEnoughForPackage(Package package, Student student);
-    public bool PackageHasCorrectDate(Package package);
-    public bool PackageDateNotTooFarInTheFutureOrPast(Package package);
-
-    public bool PackageHasCorrectStartAndEnd(Package package);
     public bool StudentCanOrderPackageOnDate(Package package, Student student);
 }
